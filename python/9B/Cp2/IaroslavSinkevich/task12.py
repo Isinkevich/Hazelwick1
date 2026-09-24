c70f23f5fe5a832d83559027c5455149192ac83c
Isinkevich <24sinkevichia@hazelwick.org>
2026-09-24 11:04:43 +0000

    24/09/26

---
+++ python/9B/Cp2/IaroslavSinkevich/task12.py
@@ -0,0 +1,5 @@
+power_table = int(input("Enter a power table to learn: "))
+i=1
+
+for i in range(1,13):
+    print(i,"to the power of", power_table,"is", power_table**i)
---
+++ python/9B/Cp2/IaroslavSinkevich/task9.py
@@ -0,0 +1,5 @@
+time_table = int(input("Enter a times table to learn: "))
+i=1
+
+for i in range(1,13):
+    print(i,"times", time_table,"is", time_table*i)
