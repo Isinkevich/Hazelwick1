96de6dc6c16adade651330b398d157f56004047e
Isinkevich <24sinkevichia@hazelwick.org>
2026-09-24 10:51:37 +0000

    24/09/26

---
+++ python/9B/Cp2/IaroslavSinkevich/task16.py
@@ -0,0 +1,4 @@
+base = float(input("Enter a base length: "))
+heig = float(input("Enter a perpendicular height length: "))
+area = base * heig/2
+print("The area of the triangle is:", area)
---
+++ python/9B/Cp2/IaroslavSinkevich/task5.py
@@ -0,0 +1,6 @@
+atrib = input("Enter a physical feature: ")
+tex = input("Enter a texture: ")
+noise = input("Enter a noise: ")
+name = input("Think of a creature name: ")
+
+print("Behold! The",tex+"-"+atrib+"ed", name, "makes a terryfing noise *"+ noise +"* as it shuffles closer!")
