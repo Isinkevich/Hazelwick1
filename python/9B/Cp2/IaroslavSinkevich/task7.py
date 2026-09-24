4c7b144cdd9e0147196905a1d2af1771291432b1
Isinkevich <24sinkevichia@hazelwick.org>
2026-09-24 10:57:00 +0000

    24/09/26

---
+++ python/9B/Cp2/IaroslavSinkevich/task7.py
@@ -0,0 +1,4 @@
+num1 = float(input("Enter a number to subtract from: "))
+num2 = float(input("Enter a number to subtract: "))
+
+print("The answer is", num1-num2)
---
+++ python/9B/Cp2/IaroslavSinkevich/task8.py
@@ -0,0 +1,4 @@
+num1 = int(input("Enter a number: "))
+num2 = int(input("Enter a number: "))
+
+print("The remainder is", num1%num2)
